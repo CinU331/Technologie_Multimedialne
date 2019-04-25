934cb5495180d2f9d1b3c0086eeee1ff411d9ed5
JavertArdo <bartekfrank18@gmail.com>
2019-04-25 14:30:10 +0200

    Updated views and fonts

--- MedicationTracker/MedicationTracker/Views/NewReminderPage.xaml.cs
+++ MedicationTracker/MedicationTracker/Views/NewReminderPage.xaml.cs
@@ -1,14 +1,26 @@
 ﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using MedicationTracker.ViewModels;
+
 namespace MedicationTracker.Views
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class NewReminderPage : ContentPage
 	{
-		public NewReminderPage()
+        NewReminderViewModel viewModel;
+
+        public NewReminderPage()
 		{
 			InitializeComponent();
-		}
-	}
+            BindingContext = viewModel = new NewReminderViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            if (viewModel.Medicines.Count == 0) { viewModel.LoadMedicinesCommand.Execute(null); }
+        }
+    }
 }
