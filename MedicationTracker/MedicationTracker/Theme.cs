0a5f55321eb5b735959a7d7c5e826ce416756ed8
JavertArdo <bartekfrank18@gmail.com>
2019-05-04 21:04:59 +0200

    Little clean up in theme style

--- MedicationTracker/MedicationTracker/Theme.cs
+++ MedicationTracker/MedicationTracker/Theme.cs
@@ -5,5 +5,6 @@ namespace MedicationTracker
     public static class Theme
     {
         public static Color Primary { get; set; } = Color.FromHex("#232426");
+        public static Color Secondary { get; set; } = Color.FromHex("#343537");
     }
 }
