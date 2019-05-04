cf71f70abdb0df0add4e3a49c0f68e213afbb517
JavertArdo <bartekfrank18@gmail.com>
2019-05-04 20:04:04 +0200

    Added simple theming example

---
+++ MedicationTracker/MedicationTracker/Theme.cs
@@ -0,0 +1,9 @@
+﻿using Xamarin.Forms;
+
+namespace MedicationTracker
+{
+    public static class Theme
+    {
+        public static Color Primary { get; set; } = Color.FromHex("#232426");
+    }
+}
